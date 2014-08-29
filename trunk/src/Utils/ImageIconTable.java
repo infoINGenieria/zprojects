1c484353adb5faa2f3c38d85db9b3e7b66781333
Matías Varela <matu.varela@gmail.com>
2014-08-29 00:40:53 -0300

    Se corrigieron las alarmas (aparecen las vencidas)
    Los permisos del rol de carga puede generar reportes. Pantalla
    de parámetros.

--- trunk/src/Utils/ImageIconTable.java
+++ trunk/src/Utils/ImageIconTable.java
@@ -4,7 +4,9 @@
  */
 package Utils;
 
+import java.awt.Color;
 import java.awt.Component;
+import java.awt.Font;
 import javax.swing.ImageIcon;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
@@ -23,15 +25,24 @@ public class ImageIconTable extends DefaultTableCellRenderer {
                                                  boolean isSelected, boolean hasFocus, 
                                                  int row, int column) {
     int opcion = Integer.valueOf(value.toString());
-    ImageIcon icon;
-    if(opcion == 0){
-        icon = new ImageIcon(getClass().
-                    getResource("/zilleprojects/resources/info-icon.png"));
-    }else {
-        icon = new ImageIcon(getClass().
+    ImageIcon icon = null;
+    switch(opcion) {
+        case 1:
+            icon = new ImageIcon(getClass().
                     getResource("/zilleprojects/resources/warn-icon.png"));
-    }  
+            break;
+        case 2:
+            icon = new ImageIcon(getClass().
+                    getResource("/zilleprojects/resources/error-icon.png"));
+            break;   
+        default:
+        case 0:
+            icon = new ImageIcon(getClass().
+                    getResource("/zilleprojects/resources/info-icon.png"));
+            break;
+    }
     setIcon(icon);
+    
     return this;
   }
 }
